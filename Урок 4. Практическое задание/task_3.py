c62348b059f725858d836216b422fcaa2da8d28b
ASh1ne <77381910+ASh1ne@users.noreply.github.com>
2021-07-01 21:41:19 +0500

    Update task_3.py

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,18 +1,5 @@
-"""
-Задание 3.
-
-Приведен код, формирующий из введенного числа
-обратное по порядку входящих в него
-цифр и вывести на экран.
-
-Сделайте профилировку каждого алгоритма через cProfile и через timeit
-
-Обязательно предложите еще свой вариант решения и также запрофилируйте!
-
-Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
-
-Без аналитики задание считается не принятым
-"""
+from timeit import timeit
+import cProfile
 
 
 def revers_1(enter_num, revers_num=0):
@@ -37,3 +24,32 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+# была использована читерская функция реверс. Первое место не заняла, но 2 все-таки захватила.
+def revers_4(enter_num):
+    num_list = list(str(enter_num))
+    num_list.reverse()
+    revers_num = "".join(num_list)
+    return revers_num
+
+
+enter_num = int(input('Введите число: '))
+
+revers_1(enter_num, revers_num=0)
+revers_2(enter_num, revers_num=0)
+revers_3(enter_num)
+revers_4(enter_num)
+
+print(timeit(f'revers_1({enter_num})', globals=globals(), number=10000))
+print(timeit(f'revers_2({enter_num})', globals=globals(), number=10000))
+print(timeit(f'revers_3({enter_num})', globals=globals(), number=10000))
+print(timeit(f'revers_4({enter_num})', globals=globals(), number=10000))
+
+cProfile.run('revers_1(1000000)')
+cProfile.run('revers_2(1000000)')
+cProfile.run('revers_3(1000000)')
+cProfile.run('revers_4(1000000)')
+
+# Из представленных варинатов решения задачи самый быстрый - срез, поскольку он имеет
+# самую простую сложность- O(n)
