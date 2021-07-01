7259836bd81e4d64624e7f053723b609c65245df
ASh1ne <77381910+ASh1ne@users.noreply.github.com>
2021-07-01 21:42:00 +0500

    Update task_4.py

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -1,21 +1,9 @@
-"""
-Задание 4.
-
-Приведены два алгоритма. В них определяется число,
-которое встречается в массиве чаще всего.
-
-Сделайте профилировку каждого алгоритма через timeit
-
-Попытайтесь написать третью версию, которая будет самой быстрой и по возможности самой лаконичной.
-Сделайте замеры и опишите, получилось ли у вас ускорить задачу.
-
-Без аналитики задание считается не принятым!
-"""
+from timeit import timeit
 
 array = [1, 3, 1, 3, 4, 5, 1]
 
 
-def func_1():
+def func_1(array):
     m = 0
     num = 0
     for i in array:
@@ -27,17 +15,24 @@ def func_1():
            f'оно появилось в массиве {m} раз(а)'
 
 
-def func_2():
+def func_2(array):
     new_array = []
     for el in array:
         count2 = array.count(el)
         new_array.append(count2)
-
     max_2 = max(new_array)
     elem = array[new_array.index(max_2)]
     return f'Чаще всего встречается число {elem}, ' \
            f'оно появилось в массиве {max_2} раз(а)'
 
 
-print(func_1())
-print(func_2())
+# выбрал самое неудачное решение, потому что сортировка это O(n log N)
+def func_3(array):
+    my_count = list(({i: array.count(i) for i in array}).items())
+    my_count.sort(key=lambda i: i[1])
+    return f'Число {my_count[-1][0]} встречается больше всего раз: {my_count[-1][1]}'
+
+
+print(timeit(f'func_1({array})', globals=globals(), number=10000))
+print(timeit(f'func_2({array})', globals=globals(), number=10000))
+print(timeit(f'func_3({array})', globals=globals(), number=10000))
